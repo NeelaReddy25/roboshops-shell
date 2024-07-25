e19a339fbd9642054b8e382118362b2fa47eeb2e
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-25 10:38:16 +0530

    good effects

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-
+domain_name="neelareddy.store"
+hosted_zone_id="Z001712433NLPH2AI8HH5"
 
 for name in ${instances[@]}; do
     if [ $name == "shipping" ] || [ $name == "mysql" ]
@@ -11,4 +12,34 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "Creating instance for: $name with instancetype: $instance_type"
+    aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance-type --security-group-ids sg-0cd5626364cf1e071 --subnet-id subnet-045b66b79d1f5cc3f --query "Instances[0].InstanceId" --output text
+
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+
+    if [ $name == "web" ]
+    then
+        aws ec2 wait instance-running --instance-ids $instance_id
+        aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text
+        ip_to_use=$public_ip
+    else
+        aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text
+        ip_to_use=$private_ip
+    fi
+
+    aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
+  {
+    "Comment": "Creating a record set for '$name'"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$name.$domain_name'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$ip_to_use'"
+        }]
+      }
+    }]
+  }'
+
 done
