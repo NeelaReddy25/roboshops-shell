2514f52c1a5bd5e9ef9922c9c358d592378861fb
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-25 09:57:52 +0530

    good effects

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,6 +3,6 @@
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 
-for name in ${intances[@]}; do
+for name in ${instances[@]}; do
     echo "Creating instance for: $name"
 done
