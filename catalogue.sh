79fd5a0ca2e7f424a472d922f55f5b3c75074162
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-25 11:59:40 +0530

    good effects

---
+++ catalogue.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FALIURE $N"
+        exit1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling Nodejs"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing Nodejs"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>>$LOGFILE
+    VALIDATE $? "Creating roboshop user"
+else
+    echo "Roboshop user already created...$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+VALIDATE $? "Downloading catalogue files"
+
+cd /app 
+rm -rf /app/*
+unzip /tmp/catalogue.zip &>>$LOGFILE
+VALIDATE $? "Extracting catalogue code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing Nodejs dependencies"
+
+cd catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+VALIDATE $? "Copied catalogue service "
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl enable catalogue &>>$LOGFILE
+VALIDATE $? "Enabling catalogue"
+
+systemctl start catalogue &>>$LOGFILE
+VALIDATE $? "Starting catalogue"
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo &>>$LOGFILE
+VALIDATE $? "Copied mongo repo"
+
+dnf install -y mongodb-mongosh &>>$LOGFILE
+VALIDATE $? "Installing MongoDB"
+
+mongosh --host mongodb.neelareddy.store </app/schema/catalogue.js &>>$LOGFILE
+VALIDATE $? "Loading MongoDB data"
