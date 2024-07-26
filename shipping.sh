fef96f0429624bf1aaace0ba43a769d559d3bcaf
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-26 22:53:24 +0530

    good effects

--- shipping.sh
+++ shipping.sh
@@ -8,7 +8,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MYSQL_HOST="mysql.neelareddy.store"
+MYSQL_SERVER="mysql.neelareddy.store"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -77,11 +77,11 @@ VALIDATE $? "Starting shipping"
 dnf install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
+mysql -h $MYSQL_SERVER -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
 if [ $? -ne 0 ]
 then
     echo "Schema is ... LOADING"
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$LOGFILE
+    mysql -h $MYSQL_SERVER -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$LOGFILE
     VALIDATE $? "Loading schema"
 else
     echo -e "Schema already exists... $Y SKIPPING $N"
