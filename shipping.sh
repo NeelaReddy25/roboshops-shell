b7aebe4b727d6b118a6703a8a61ac1b69b85579d
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-26 20:19:03 +0530

    good effects

--- shipping.sh
+++ shipping.sh
@@ -179,15 +179,18 @@ VALIDATE $? "Starting shipping"
 dnf install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
-if [ $? -ne 0 ]
-then
-    echo "Schema is ... LOADING"
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
-    VALIDATE $? "Loading schema"
-else
-    echo -e "Schema already exists... $Y SKIPPING $N"
-fi
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql  &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+# mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
+# if [ $? -ne 0 ]
+# then
+#     echo "Schema is ... LOADING"
+#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+#     VALIDATE $? "Loading schema"
+# else
+#     echo -e "Schema already exists... $Y SKIPPING $N"
+# fi
 
 systemctl restart shipping &>> $LOGFILE
 VALIDATE $? "Restarted Shipping"
